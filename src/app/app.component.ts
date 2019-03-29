45aecd93db57884d0493e9495ceeedd80ec7f151
Andre MacNamara <andremacnamara12@gmail.com>
2019-03-29 19:46:08 +0000

    Tidy up. Proper link the API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
@@ -12,24 +11,23 @@ import { TestService } from './test.service';
 export class AppComponent implements OnInit {
   baseUrl = environment.apiUrl;
   title = 'PropertyAdverts-Client';
-  names: any;
-  state: any;
 
-  constructor (private http: HttpClient, private test: TestService) {}
+  values: any;
+
+
+  constructor (private http: HttpClient) {}
 
   ngOnInit() {
-    this.dataService();
+    this.getValues();
   }
 
-  dataService() {
-    this.test.dataService().subscribe(data => {
-      // this.names = data['name'];
-      console.log('T' + JSON.stringify(data));
+  getValues() {
+    this.http.get('http://127.0.0.1:8000/api/properties').subscribe(response => {
+      this.values = response;
     }, error => {
-      console.log('F' + JSON.stringify(error));
+      console.log(error);
     });
   }
-
 
 }
 
