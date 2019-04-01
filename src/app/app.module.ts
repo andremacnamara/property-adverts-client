0e3760dfb84145417d285bac1c8f2cf665c62089
Andre MacNamara <andremacnamara12@gmail.com>
2019-04-01 17:22:26 +0100

    Alertify configured.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,14 @@
+import { appRoutes } from './routes';
+
+// Components
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -5,15 +16,10 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NavComponent } from './nav/nav.component';
-import { RegisterComponent } from './register/register.component';
-
+// Services
 import { AuthService } from './_services/auth.service';
-import { appRoutes } from './routes';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HomeComponent } from './home/home.component';
+import { AlertifyService } from './_services/alertify.service';
+
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -40,10 +46,11 @@ export function tokenGetter() {
             whitelistedDomains: ['http://127.0.0.1:8000'],
             blacklistedRoutes: ['http://127.0.0.1:8000']
          }
-      }),
+      })
    ],
    providers: [
-      AuthService
+      AuthService,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
